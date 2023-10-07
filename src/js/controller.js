269469390aea60b077b61adb1571f9c89e3a4d0c
AndSim <siminiucandrew@gmail.com>
2023-10-07 13:49:22 +0300

    feat: search functionality - 1 handling the event of a user searching for recipes, 2 loading the search results and 3 rendering all results. clear search field. changing from truly private to only protected. error message for search result not found.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,17 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/actual';
 import 'regenerator-runtime/runtime';
+import { state } from './model.js';
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+  module.hot.accept();
+}
 
-const controlRecipes = async function () {
+const controlRecipes = async function() {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
@@ -20,8 +25,24 @@ const controlRecipes = async function () {
   }
 };
 
-const init = function() {
-  recipeView.addHandlerRender(controlRecipes);
+const controlSearchResults = async function() {
+  try {
+    resultsView.renderSpinner();
+
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
 init();
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,8 +1,8 @@
 import { TIMEOUT_SEC } from './config.js';
 
-export const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
+export const timeout = function(s) {
+  return new Promise(function(_, reject) {
+    setTimeout(function() {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
