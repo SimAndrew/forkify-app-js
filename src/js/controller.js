7e944644e7481866ec82df786406cf91827c5943
AndSim <siminiucandrew@gmail.com>
2023-10-08 16:43:54 +0300

    feat: add pagination of results of searching recipes, make 10 results on page.

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,14 +2,16 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import PaginationView from './views/paginationView.js';
 
 import 'core-js/actual';
 import 'regenerator-runtime/runtime';
 import { state } from './model.js';
+import paginationView from './views/paginationView.js';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function() {
   try {
@@ -34,15 +36,25 @@ const controlSearchResults = async function() {
 
     await model.loadSearchResults(query);
 
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    paginationView.render(model.state.search);
   } catch (err) {
+    alert(err);
+
     console.log(err);
   }
 };
 
+const controlPagination = function(goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  paginationView.render(model.state.search);
+};
 
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
